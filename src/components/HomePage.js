a7dff84adbcf2796b9f6457806280761ab8d4f03
Huu Thuan <thuannh0206@gmail.com>
2019-05-27 11:49:44 +0700

    [INTERNAL] Remove redundant Exercises

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
 
-import { Button, ButtonGroup } from 'reactstrap';
-import Ex1 from './Exer1';
-import Ex2 from './Exer2';
 import Ex3 from './Exer3';
 import './style.css';
 
@@ -22,22 +19,9 @@ class HomePage extends Component{
     }
 
     render(){
-        let sCurrentUI=this.state.sCurrentUI;
-        let showingUI;
-        if (sCurrentUI && sCurrentUI==="Ex1"){
-            showingUI=<Ex1/>
-        } else if (sCurrentUI && sCurrentUI==="Ex2"){
-            showingUI=<Ex2/>
-        } else if (sCurrentUI && sCurrentUI ==="Ex3"){
-            showingUI=<Ex3/>
-        }
+        let showingUI=<Ex3/>
         return (
             <div className="d-flex flex-column App">
-                <ButtonGroup size="lg">
-                    <Button value="Ex1" onClick={this.onClickBtn.bind(this)}>Ex. 1</Button>
-                    <Button value="Ex2" onClick={this.onClickBtn.bind(this)}>Ex. 2</Button>
-                    <Button value="Ex3" onClick={this.onClickBtn.bind(this)}>Ex. 3</Button>
-                </ButtonGroup>
                 {showingUI}
             </div>
         )
